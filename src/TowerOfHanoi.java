eda029cdb1b0cb6678633d8dd6f648ee6991efe3
ps29653 <SharmaPrashant@JohnDeere.com>
2021-01-10 20:58:49 +0530

    Solution to Tower of Hanoi

---
+++ src/TowerOfHanoi.java
@@ -0,0 +1,18 @@
+package src;
+
+public class TowerOfHanoi {
+
+    public static String hanoi(int nunOfDisks, int fromTower, int toTower) {
+
+        if (nunOfDisks == 1) return fromTower + " -> " + toTower + ";";
+        else {
+            String move1, move2, move3;
+            int otherTower = 6 - fromTower - toTower;
+            move1 = hanoi(nunOfDisks - 1, fromTower, otherTower);
+            move2 = fromTower + " -> " + toTower + ";";
+            move3 = hanoi(nunOfDisks - 1, otherTower, toTower);
+
+            return move1 + move2 + move3;
+        }
+    }
+}
