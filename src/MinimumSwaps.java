6ee9551c3378f944132c723f078727a1c4935eb8
ps29653 <SharmaPrashant@JohnDeere.com>
2021-04-12 10:04:09 +0530

    minimum-swaps

---
+++ src/MinimumSwaps.java
@@ -0,0 +1,26 @@
+package src;
+
+/**
+ * Problem URL: https://aonecode.com/amazon-online-assessment-algorithm-swap
+ */
+public class MinimumSwaps {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 4, 1, 2};
+        System.out.println(numberOfSwap(arr));
+
+    }
+
+    public static int numberOfSwap(int[] num) {
+
+        int swaps = 0;
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i; j < num.length; j++)
+                if (num[i] > num[j]) swaps++;
+        }
+
+        return swaps;
+    }
+
+}
