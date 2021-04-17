b1d780cd71b259eb45cb6af6d34637624fff74d7
ps29653 <SharmaPrashant@JohnDeere.com>
2021-04-17 21:46:27 +0530

    run-length-encoding

---
+++ src/stringproblems/RunLengthEncoding.java
@@ -0,0 +1,33 @@
+package src.stringproblems;
+
+public class RunLengthEncoding {
+
+    public static void main(String[] args) {
+
+        System.out.println(encode("aabbcffgd"));
+    }
+
+    public static String encode(String str) {
+
+        if (str.length() == 1) return str + 1;
+
+        StringBuilder sb = new StringBuilder();
+        int counter = 1;
+        char currChar = ' ';
+        char nextChar = ' ';
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            currChar = str.charAt(i);
+            nextChar = str.charAt(i + 1);
+            if (currChar == nextChar) counter++;
+            else {
+                sb.append(currChar).append(counter);
+                counter = 1;
+            }
+        }
+
+        sb.append(nextChar).append(counter);
+
+        return sb.toString();
+    }
+}
