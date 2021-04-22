b77f5d40f4881254d2a6e0c5c959c27abe63b5cb
ps29653 <SharmaPrashant@JohnDeere.com>
2021-04-22 21:07:06 +0530

    longest-palindromic-substring

--- src/stringproblems/LongestPalindromeSubString.java
+++ src/stringproblems/LongestPalindromeSubString.java
@@ -1,44 +1,44 @@
 package src.stringproblems;
 
+import static java.lang.Math.max;
+
+/**
+ * Problem URL: https://leetcode.com/problems/longest-palindromic-substring/
+ */
 public class LongestPalindromeSubString {
 
     public static void main(String[] args) {
 
-        System.out.println(getLongestPalindromeSubString("bbb"));
+        System.out.println(getLongestPalindromeSubString("bb"));
     }
 
     public static String getLongestPalindromeSubString(String s) {
 
         if (s == null || s.length() < 1) return "";
 
-        int start = 0, end = 0;
-
+        int startIndex = 0;
+        int endIndex = 0;
         for (int i = 0; i < s.length(); i++) {
-
-            int len1 = expandAroundCenter(s, i, i);
-
-            int len2 = expandAroundCenter(s, i, i + 1);
-
-            int len = Math.max(len1, len2);
-
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+            int len = max(expandAroundCenter(s, i, i), expandAroundCenter(s, i, i + 1));
+            if (len > endIndex - startIndex) {
+                startIndex = i - (len - 1) / 2;
+                endIndex = i + len / 2;
             }
         }
 
-        return s.substring(start, end + 1);
+        return s.substring(startIndex, endIndex + 1);
     }
 
     private static int expandAroundCenter(String s, int left, int right) {
 
-        int L = left, R = right;
+        int tempLeft = left;
+        int tempRight = right;
 
-        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-            L--;
-            R++;
+        while (tempLeft >= 0 && tempRight < s.length() && s.charAt(tempLeft) == s.charAt(tempRight)) {
+            tempLeft--;
+            tempRight++;
         }
 
-        return R - L - 1;
+        return tempRight - tempLeft - 1;
     }
 }
