a805d3bbe509d4414610593aac2b12cccce30893
ps29653 <SharmaPrashant@JohnDeere.com>
2021-04-22 11:20:28 +0530

    nearest-cities

--- src/NearestCities.java
+++ src/NearestCities.java
@@ -2,10 +2,14 @@ package src;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+
+import static java.lang.Math.abs;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.IntStream.range;
 
 /**
  * Problem URL: https://aonecode.com/amazon-online-assessment-nearest-cities
@@ -15,14 +19,15 @@ public class NearestCities {
 
     public static void main(String[] args) {
 
-        int numOfPoints = 3;
+        int numOfPoints = 6;
         String[] points = {"green", "yellow", "red", "blue", "grey", "pink"};
         int[] xCoordinates = {10, 20, 15, 30, 10, 15};
         int[] yCoordinates = {30, 25, 30, 40, 25, 25};
         int numOfQueriedPoints = 4;
         String[] queriedPoints = {"grey", "blue", "red", "pink"};
 
-        System.out.println(Arrays.toString(findNearestCities(numOfPoints, points, xCoordinates, yCoordinates, numOfQueriedPoints, queriedPoints)));
+        System.out.println(Arrays.toString(findNearestCities(numOfPoints, points, xCoordinates, yCoordinates,
+                numOfQueriedPoints, queriedPoints)));
 
     }
 
@@ -36,85 +41,55 @@ public class NearestCities {
         String[] res = new String[numOfQueriedPoints];
         int index = 0;
 
-        Map<String, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < points.length; i++) {
-            String point = points[i];
-            List<Integer> cor = new ArrayList<>(2);
-            cor.add(xCoordinates[i]);
-            cor.add(yCoordinates[i]);
-            map.put(point, cor);
-        }
+        Map<String, List<Integer>> pointsCoOrdinateMap = mapPointsToCoOrdinates(points, xCoordinates, yCoordinates);
 
-        for (String s : queriedPoints) {
+        for (String queriedPoint : queriedPoints) {
 
-            List<Integer> find = map.get(s);
+            List<Integer> queriedPointCoOrdinates = pointsCoOrdinateMap.get(queriedPoint);
             List<String> matchingPoints = new ArrayList<>();
+            findMatchingPoints(pointsCoOrdinateMap, queriedPoint, queriedPointCoOrdinates, matchingPoints);
+            TreeMap<Integer, String> sortedMap = sortByNearestPoint(pointsCoOrdinateMap, queriedPointCoOrdinates, matchingPoints);
 
-            for (Map.Entry<String, List<Integer>> e : map.entrySet()) {
-
-                if (!e.getKey().equalsIgnoreCase(s)) {
-
-                    boolean xMatching = false;
-                    boolean yMatching = false;
-                    if (e.getValue().get(0).intValue() == find.get(0).intValue()) xMatching = true;
-                    if (e.getValue().get(1).intValue() == find.get(1).intValue()) yMatching = true;
-                    if (xMatching || yMatching) matchingPoints.add(e.getKey());
-
-                }
-
-            }
-
-            TreeMap<Integer, String> fMap = new TreeMap<>();
-            for (String mp : matchingPoints) {
-                List<Integer> check = map.get(mp);
-                int dis = Math.abs(check.get(0) - find.get(0)) + Math.abs(check.get(1) - find.get(1));
-                fMap.put(dis, mp);
-            }
-
-            if (!fMap.isEmpty()) {
-                res[index] = fMap.firstEntry().getValue();
-                index++;
-            } else {
-                res[index] = null;
-                index++;
-            }
+            if (sortedMap.isEmpty()) res[index] = null;
+            else res[index] = sortedMap.firstEntry().getValue();
+            index++;
         }
 
         return res;
     }
-}
 
-class Point implements Comparable<Point> {
-    String name;
-    int x;
-    int y;
-    int d;
+    private static TreeMap<Integer, String> sortByNearestPoint(Map<String, List<Integer>> map,
+                                                               List<Integer> find,
+                                                               List<String> matchingPoints) {
 
-    Point(String name, int x, int y) {
-        this.name = name;
-        this.x = x;
-        this.y = y;
+        return matchingPoints.stream().collect(toMap(mp ->
+                        abs(map.get(mp).get(0) - find.get(0)) + abs(map.get(mp).get(1) - find.get(1)),
+                mp -> mp, (a, b) -> b, TreeMap::new));
     }
 
-    Point(String name, int x, int y, int d) {
-        this.name = name;
-        this.x = x;
-        this.y = y;
-        this.d = d;
+    private static void findMatchingPoints(Map<String, List<Integer>> map,
+                                           String queriedPoint,
+                                           List<Integer> find,
+                                           List<String> matchingPoints) {
+
+        map.forEach((key, value) -> {
+            if (!key.equalsIgnoreCase(queriedPoint) &&
+                    (compareCoOrdinate(find, value, 0) || compareCoOrdinate(find, value, 1)))
+                matchingPoints.add(key);
+        });
     }
 
-    public int compareTo(Point p) {
-        return this.d - p.d;
+    private static boolean compareCoOrdinate(List<Integer> find, List<Integer> in, int index) {
+
+        return in.get(index).intValue() == find.get(index).intValue();
     }
 
-    public int getXDistance(Point p) {
+    private static Map<String, List<Integer>> mapPointsToCoOrdinates(String[] points,
+                                                                     int[] xCoordinates,
+                                                                     int[] yCoordinates) {
 
-        return Math.abs(this.x - p.x);
-    }
-
-    public int getYDistance(Point p) {
-
-        return Math.abs(this.y - p.y);
+        return range(0, points.length).boxed().collect(toMap(i -> points[i], i ->
+                asList(xCoordinates[i], yCoordinates[i]), (a, b) -> b));
     }
 
 }
