e641d84d54ab94f1c69915bb7cabdf6c2cd35c53
Prashant Sharma <prashant.sharma1thoughtworks.com>
2021-11-02 08:17:45 +0530

    excel-sheet-column-number

--- src/stringproblems/ExcelNumber.java
+++ src/stringproblems/ExcelNumber.java
@@ -9,22 +9,20 @@ import java.util.Map;
 public class ExcelNumber {
 
     public static void main(String[] args) {
-        System.out.println(titleToNumber("FXSHRXW"));
+        System.out.println(titleToNumber("A"));
     }
 
     public static int titleToNumber(String columnTitle) {
 
         int excelNumber = 1;
         String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        Map<String, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray()) map.put(String.valueOf(c), excelNumber++);
-
-        if (map.containsKey(columnTitle)) return map.get(columnTitle);
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) map.put(c, excelNumber++);
 
         excelNumber = 0;
         int columnTitleLength = columnTitle.length();
         for (int i = columnTitleLength - 1; i >= 0; i--) {
-            excelNumber = excelNumber + (int) (Math.pow(26, (columnTitleLength - i - 1)) * map.get(String.valueOf(columnTitle.charAt(i))));
+            excelNumber = excelNumber + (int) (Math.pow(26, (columnTitleLength - i - 1)) * map.get(columnTitle.charAt(i)));
         }
 
         return excelNumber;
